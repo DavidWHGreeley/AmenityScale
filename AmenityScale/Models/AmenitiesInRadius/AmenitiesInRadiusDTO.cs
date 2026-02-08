a75350f55801c25d3f126f488e6d3ecf2836585c
David Greeley <43015132+DavidWHGreeley@users.noreply.github.com>
2026-02-07 21:35:46 -0500

    Removed old project Added SP for radius search

---
+++ AmenityScale/Models/AmenitiesInRadius/AmenitiesInRadiusDTO.cs
@@ -0,0 +1,14 @@
+﻿using AmenityScale.Models.Amenity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmenityScale.Models.AmenitiesInRadius
+{
+    public class AmenitiesInRadiusDTO : AmenityDTO
+    {
+        public double DistanceInMeters { get; set; }
+    }
+}
