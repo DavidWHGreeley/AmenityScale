76cf4d33a3fd2365f371c900a30ef2671dc47df9
David Greeley <43015132+DavidWHGreeley@users.noreply.github.com>
2026-02-21 11:20:18 -0500

    Comment v1
    Index and DTO got flowerboxes

--- AmenityScaleCore/Models/AmenitiesInRadius/AmenitiesInRadiusDTO.cs
+++ AmenityScaleCore/Models/AmenitiesInRadius/AmenitiesInRadiusDTO.cs
@@ -1,4 +1,9 @@
 ﻿using AmenityScaleCore.Models.Amenity;
+/// <summary>
+/// Version         Date        Coder                   Remarks
+/// 0.1             2026-07-02  Greeley                 Response DTO from the database Get amenities in radius
+/// 0.2             2026-20-02  Greeley                 Updated cause server is returning if it's negative amenity
+/// 
 
 namespace AmenityScaleCore.Models.AmenitiesInRadius
 {
